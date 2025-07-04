1b61d769526add4b1d63d32a7910ce1da54f0d81
2351050186tram@ou.edu.vn <2351050186tram@.ou.edu.vn>
2025-07-04 11:33:04 +0700

    Cập nhật nội dung README.md

--- QuizApp/src/main/java/module-info.java
+++ QuizApp/src/main/java/module-info.java
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
+
 module com.dht.quizapp {
-=======
-module com.ndtt.quizapp {
->>>>>>> 0981571c89f5c611c1505c26adaca7e17d48eb47
-    requires javafx.controls;
+
+   requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
     requires lombok;
-<<<<<<< HEAD
     requires java.sql;
-
+    
     opens com.dht.quizapp to javafx.fxml;
     exports com.dht.quizapp;
-=======
-
-    opens com.ndtt.quizapp to javafx.fxml;
-    exports com.ndtt.quizapp;
->>>>>>> 0981571c89f5c611c1505c26adaca7e17d48eb47
 }
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
+
 # QuizApp
-=======
-# QuizApp
->>>>>>> 0981571c89f5c611c1505c26adaca7e17d48eb47
+
